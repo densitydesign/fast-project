f74ded95d8e0590207c35881b8b49ea022f5e523
Enrico <edeusebio@cgnal.com>
2018-10-06 17:15:27 +0200

    Filtering on Timestamps, Content and Limiting. Content Classification LIFESTYLE, FASHION and AMBIENT

--- api/app/app.py
+++ api/app/app.py
@@ -3,6 +3,7 @@ from flask_pymongo import PyMongo
 from flask_restful import Api, Resource
 from flasgger import Swagger, swag_from
 from flask_cors import CORS
+from flask import request
 
 app = Flask(__name__)
 swagger = Swagger(app)
@@ -27,25 +28,48 @@ class Brand(Resource):
         result = list(cursor)
         return jsonify(result)
 
-
+from flask_restful.reqparse import RequestParser
+        
 class Post(Resource):
+    parser = RequestParser()
+
+    parser.add_argument('start', type=int)
+    parser.add_argument('end'  , type=int)
+    parser.add_argument('limit', type=int)
+    parser.add_argument('content', type=str)
+    
     @swag_from("yamls/post.yml")
-    def get(self, brand_id, limit=0):
-        cursor = mongo.db.post.find(
-            {
+    def get(self, brand_id):
+
+        args = self.parser.parse_args()
+
+        query = {
                 "owner": brand_id
-            },
-            {
-                "_id": 0
-            }).limit(limit)
+            }
+            
+        limit = args.limit if (args.limit) else 10
+
+        time_query = {}
+        if (args.start):
+            time_query["$gte"] = args.start
+        if (args.end):
+            time_query["$lte"] = args.end
+        if len(time_query)>0:
+            query["taken_at_timestamp"] = time_query
+            
+        if (args.content):
+            query["main_content"] = args.content
+
+        print(query)
+            
+        cursor = mongo.db.post.find(query,{"_id": 0}).limit(limit)
         result = list(cursor)
         return jsonify(result)
 
 
 api = Api(app)
 api.add_resource(Brand, "/brands")
-api.add_resource(Post, "/posts/<brand_id>", "/posts/<string:brand_id>/limit/<int:limit>")
-
+api.add_resource(Post, "/posts/<brand_id>", "/posts/<string:brand_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
