8eb8e6a3791aebbd3d793b900ab23f48eb80c846
gaspa93 <mattiagasparini2@gmail.com>
2018-11-15 13:10:26 +0000

    updated import script

--- data-ingestion/insert_followers_post.sh
+++ data-ingestion/insert_followers_post.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # insert followers posts
-sed 's/\\\"//g' <../../csv/post.csv >../../csv/post_fixed.csv
+sed 's/\\\"//g' <../../csv/followers/post.csv >../../csv/followers/post_fixed.csv
 ./convert_2.py followers
-mongoimport --db FaST --collection post_followers --mode insert --file ../../../json/post.json  --jsonArray
+mongoimport --db FaST --collection post_followers --mode insert --file ../../json/post.json  --jsonArray
 rm -rf ../../json
