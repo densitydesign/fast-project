0e7d2de535a4d8501be17adb349f99e551fccb70
Domenico Cosentino <d.cosentino@4sigma.it>
2018-11-26 14:38:04 +0100

    Impostata pagina per la definizione definizione delle community + collegamento con le api per i dati delle community

--- webui/sito/urls.py
+++ webui/sito/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
+from django.conf.urls import *
 from django.urls import path
 from sito import views
 
 urlpatterns = [
+    path('change-section/', views.change_section, name='change-section'),
     path('brand/', views.home, name='brand'),
+    path('community-definition/', views.community_definition, name='community-definition'),
+    url('community/(?P<id_community>\w+)', views.community_detail, name='community-detail'),
     path('community/', views.community, name='community'),
-    # path('community/<str:category>/$', views.CommunityDetail, name='community-detail'),
-    path('community/test/', views.CommunityDetail, name='community-detail'),
+    #path('community/test/', views.community_detail, name='community-detail'),
 ]
--- webui/sito/views.py
+++ webui/sito/views.py
@@ -1,11 +1,61 @@
-from django.shortcuts import render
+import json, pprint, random, requests
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
 def home(request):
-    return render(request, 'sito/brand.html')
+    return render(request, 'sito/brand.html', {'section':'brand'})
+
+
+def community_definition(request):
+    if request.method == 'POST':
+        # Per il momento mettiamo gli id in sessione
+        request.session['community_id'] = request.POST.getlist('community_id')
+        return HttpResponseRedirect(reverse('community'))
+    url = 'http://localhost:8080/communities'
+    response = requests.get(url)
+    elenco = []
+    if (response.ok):
+        data = json.loads(response.content)
+        for d in data:
+            d['id'] = '%d' %d['id']
+            elenco.append(d)
+    # TODO: gestire nomi custom
+    return render(request, 'sito/community_definition.html', {'section':'community', 'elenco': elenco})
 
 def community(request):
-    return render(request, 'sito/community.html')
+    community_ids = ','.join(request.session.get('community_id', []))
+    url = 'http://localhost:8080/communities/graph?communities=%s&limit=10'% community_ids
+    response = requests.get(url)
+    edges = []
+    nodes = []
+    if (response.ok):
+        data = json.loads(response.content)
+        for d in data['edges']:
+            edges.append(d)
 
-def CommunityDetail(request):
+        # Trovo il numero massimo di post per poter dimensionare i nodi
+        maxPosts = 0 
+        for d in data['nodes']:
+            p = d.get('num_posts', 1)
+            if p > maxPosts:
+                maxPosts = p
+
+        for d in data['nodes']:
+            if d['id'][0] == 'u':
+                d['color'] = '#c6efef'
+            else:
+                d['color'] = "#C7BBD5" 
+            d['size'] = (d.get('num_posts', maxPosts) / maxPosts) * 100
+            nodes.append(d)
+        random.shuffle(nodes)
+    return render(request, 'sito/community.html', {'section':'community', 'edges': edges, 'nodes':nodes})
+
+def community_detail(request, id_community):
     # print (category)
-    return render(request, 'sito/community-detail.html')
+    return render(request, 'sito/community-detail.html', {'section':'community'})
+
+
+def change_section(request):
+    if request.POST.get('switch', '') == 'community':
+        return HttpResponseRedirect(reverse('community-definition'))
+    return HttpResponseRedirect(reverse('brand'))
