6411a4361b07618c1f3613945423f89ad2fee119
Domenico Cosentino <d.cosentino@4sigma.it>
2019-01-03 11:26:39 +0100

    Mega commit pre review

--- webui/sito/urls.py
+++ webui/sito/urls.py
@@ -4,10 +4,15 @@ from django.urls import path
 from sito import views
 
 urlpatterns = [
+    path('imposta-filtri/', views.imposta_filtri, name='imposta-filtri'),
     path('change-section/', views.change_section, name='change-section'),
-    path('brand/', views.home, name='brand'),
+    url('brand-hashtags', views.brand_hashtags, name='brand-hashtags'),
+    url('brand-time', views.brand_time, name='brand-time'),
+    url('brand-content', views.brand_content, name='brand-content'),
     path('community-definition/', views.community_definition, name='community-definition'),
-    url('community/(?P<id_community>\w+)', views.community_detail, name='community-detail'),
+    url('community/(?P<id_community>[0-9]+)/(?P<tipo>.*)?$', views.community_detail, name='community-detail'),
+    url('community/(?P<id_community>[0-9]+)', views.community_detail, name='community-detail'),
     path('community/', views.community, name='community'),
     #path('community/test/', views.community_detail, name='community-detail'),
+    path('', views.brand_content, name='brand'),
 ]
--- webui/sito/views.py
+++ webui/sito/views.py
@@ -1,9 +1,32 @@
 import json, pprint, random, requests
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
-def home(request):
+
+@csrf_exempt
+def imposta_filtri(request):
+    print('imposta_filtri')
+    print (request.POST)
+    request.session['brand'] = request.POST.get('brand', None)
+    request.session['competitor'] = request.POST.get('competitor', None)
+    request.session['complexity'] = request.POST.get('complexity', '100')
+    request.session['limit'] = request.POST.get('limit', '10')
+    request.session['timeframe'] = request.POST.get('timeframe', request.session.get('timeframe', ''))
+    request.session['start'] = request.POST.get('start', '')
+    request.session['end'] = request.POST.get('end', '')
+
+    return HttpResponse('OK')
+
+
+def brand_content(request):
     return render(request, 'sito/brand.html', {'section':'brand'})
+
+def brand_time(request):
+    return render(request, 'sito/brand.html', {'section':'brand'})
+
+def brand_hashtags(request):
+    return render(request, 'sito/brand_hashtags.html', {'section':'brand'})
 
 
 def community_definition(request):
@@ -43,16 +66,178 @@ def community(request):
         for d in data['nodes']:
             if d['id'][0] == 'u':
                 d['color'] = '#c6efef'
+                d['size'] = 20
             else:
                 d['color'] = "#C7BBD5" 
-            d['size'] = (d.get('num_posts', maxPosts) / maxPosts) * 100
+                d['size'] = (d.get('num_posts', maxPosts) / maxPosts) * 100
             nodes.append(d)
         random.shuffle(nodes)
     return render(request, 'sito/community.html', {'section':'community', 'edges': edges, 'nodes':nodes})
 
-def community_detail(request, id_community):
-    # print (category)
-    return render(request, 'sito/community-detail.html', {'section':'community'})
+def community_detail(request, id_community, tipo='influencers'):
+    brand = request.session.get('brand', '2252447111')
+    competitor = request.session.get('competitor', None)
+    limit = request.session.get('limit', '10')
+    complexity = request.session.get('complexity', '100')
+    start = request.session.get('start', '')
+    end = request.session.get('end', '')
+    if start:
+        start = '&start=%s' % start
+    if end:
+        end = '&end=%s' % end
+    data = None
+    data_competitor = None
+    timeframe = request.session.get('timeframe', '')
+    start_day = ''
+    end_day = ''
+    if timeframe:
+        a = timeframe.split(' - ')
+        start_day = a[0]
+        end_day = a[1]
+    if tipo == 'influencers':
+        url = 'http://localhost:8080/communities/graph?communities=%s&limit=%s%s%s'% (id_community, limit, start, end)
+        response = requests.get(url)
+        nodes = []
+        if (response.ok):
+            data = json.loads(response.content)
+            # Trovo la dimensione massima
+            max_post = 0
+            for d in data['nodes']:
+                if d['label'] == 'user':
+                    if d['num_posts'] > max_post:
+                        max_post = d['num_posts']
+            for d in data['nodes']:
+                if d['label'] == 'user':
+                    num = 100 * d['num_posts'] / max_post
+                    if num >= 75:
+                        className = 'f1'
+                    elif num >= 50:
+                        className = 'f2'
+                    elif num >= 50:
+                        className = 'f3'
+                    elif num >= 0:
+                        className = 'f4'
+                    nodes.append({
+                        'title': d['username'],
+                        'size': d['followers_count'],
+                        'class': className
+                    })
+    elif tipo == 'hashtags':
+        # TODO: applicare filtri temporali
+        url = 'http://localhost:8080/communities/%s/%s/hashtags?limit=%s&complexity=%s%s%s' % (id_community, brand, limit, complexity, start, end)
+        response = requests.get(url)
+        nodes = []
+        if (response.ok):
+            data = json.loads(response.content)
+            max_users = 0
+            for k, v in data['hashtags'].items():
+                if v > max_users:
+                    max_users = v
+            
+        if competitor:
+            url = 'http://localhost:8080/communities/%s/%s/hashtags?limit=%s&complexity=%s%s%s' % (id_community, competitor, limit, complexity, start, end)
+            response = requests.get(url)
+            nodes = []
+            if (response.ok):
+                data_competitor = json.loads(response.content)
+                max_users = 0
+                for k, v in data_competitor['hashtags'].items():
+                    if v > max_users:
+                        max_users = v
+                
+        for k, v in data['hashtags'].items():
+            num = 100 * v / max_users
+            if num >= 75:
+                className = 'f1'
+            elif num >= 50:
+                className = 'f2'
+            elif num >= 50:
+                className = 'f3'
+            elif num >= 0:
+                className = 'f4'
+            nodes.append({
+                'title': k,
+                'size': v,
+                'class': className
+            })
+
+        if competitor:
+            for k, v in data_competitor['hashtags'].items():
+                num = 100 * v / max_users
+                if num >= 75:
+                    className = 'c1'
+                elif num >= 50:
+                    className = 'c2'
+                elif num >= 50:
+                    className = 'c3'
+                elif num >= 0:
+                    className = 'c4'
+                nodes.append({
+                    'title': k,
+                    'size': v,
+                    'class': className
+                })
+        random.shuffle(nodes)
+    elif tipo == 'mentions':
+        url = 'http://localhost:8080/communities/%s/%s/mentions?limit=%s&complexity=%s%s%s' % (id_community, brand, limit, complexity, start, end)
+        response = requests.get(url)
+        nodes = []
+        if (response.ok):
+            data = json.loads(response.content)
+            max_users = 0
+            for k, v in data['mentions'].items():
+                if v > max_users:
+                    max_users = v
+        else:
+            print(response.content)  
+        if competitor:
+            url = 'http://localhost:8080/communities/%s/%s/mentions?limit=%s&complexity=%s%s%s' % (id_community, competitor, limit, complexity, start, end)
+            response = requests.get(url)
+            nodes = []
+            if (response.ok):
+                data_competitor = json.loads(response.content)
+                max_users = 0
+                for k, v in data_competitor['mentions'].items():
+                    if v > max_users:
+                        max_users = v
+        for k, v in data['mentions'].items():
+                num = 100 * v / max_users
+                if num >= 80:
+                    className = 'f1'
+                elif num >= 60:
+                    className = 'f2'
+                elif num >= 40:
+                    className = 'f3'
+                elif num >= 20:
+                    className = 'f4'
+                elif num >= 20:
+                    className = 'f5'
+                nodes.append({
+                    'title': k,
+                    'size': v,
+                    'class': className
+                })
+        if competitor and data_competitor:
+            for k, v in data_competitor['mentions'].items():
+                num = 10 * v / max_users
+                if num >= 8:
+                    className = 'c1'
+                elif num >= 6:
+                    className = 'c2'
+                elif num >= 4:
+                    className = 'c3'
+                elif num >= 2:
+                    className = 'c4'
+                else:
+                    className = 'c5'
+                nodes.append({
+                    'title': k,
+                    'size': v,
+                    'class': className
+                })
+        random.shuffle(nodes)
+    return render(request, 'sito/community-detail.html', {'section':'community', 'id_community': id_community, 'data':nodes, 'start_day': start_day,
+            'end_day':end_day, 'tipo':tipo})
 
 
 def change_section(request):
